c7231b947102d6fc18493121bf6b2005d102794c
Mauro Costa <mauro@cuponeria.com.br>
2022-04-11 10:15:38 -0300

    Correções react

--- src/pages/components/header.js
+++ src/pages/components/header.js
@@ -6,10 +6,10 @@ class Header extends Component {
     
         return(
             <div>
-                <nav class="navbar navbar-expand-lg navbar-light bg-dark"> 
-                    <a class="navbar-brand text-warning" href="/">MyClient</a>
-                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
+                <nav className="navbar navbar-expand-lg navbar-light bg-dark"> 
+                    <a className="navbar-brand text-warning mx-5" href="/">MyClient</a>
+                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
                     </button>
 
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
